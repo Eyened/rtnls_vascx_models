c9bcaaae7e74505c561bb68a26005df52e6d6cdb
Jose <j.vargasquiros@erasmusmc.nl>
2025-07-31 15:30:05 +0200

    updating deps

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup(
         ]
     },
     install_requires=[
-        "numpy == 1.*",
+        "numpy == 2.*",
         "pandas == 2.*",
         "tqdm == 4.*",
-        "Pillow == 9.*",
+        "Pillow == 11.*",
         "click==8.*",
     ],
     python_requires=">=3.10, <3.11",
